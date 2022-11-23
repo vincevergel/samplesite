dda5b8962d7b2c8d6f8f4a7b7cba24d164b3dfc4
vincevergel <92933426+vincevergel@users.noreply.github.com>
2022-11-24 01:49:33 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# samplesite
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/pages/Events.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Events() {
+  return <h1 className='events'>EVENTS</h1>;
+}
